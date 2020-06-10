f44a782ed87231fb3a407815dcd9e4de62424cfb
Bruno <bvaugusto@gmail.com>
2020-06-09 21:54:48 -0300

    Adjust layout Modal Consultar a cadeia por id

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,9 +1,9 @@
 var globContract = false;
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
   //instancia o web3
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
-  
+
   //define conta padrão
   web3.eth.defaultAccount = web3.eth.coinbase;
 
@@ -454,7 +454,7 @@ window.addEventListener("load", function(){
   ]);
 
   //aponta para publicado
-  globContract = contractABI.at('0x1B7401882C5c638819c38b85dC7Aea16b31092d6');
+  globContract = contractABI.at('0x775efddeb4c223207ce69f3ee362c9e5543dc2db');
 
   buildChainsTable();
 
@@ -467,13 +467,13 @@ function buildChainsTable() {
       let rows = "filipe";
 
       $.each(result, function (index, value) {
-        globContract.getNextAction(value, function (error, result){
+        globContract.getNextAction(value, function (error, result) {
           let tr = "";
 
           if (!error) {
             if (result == 'MANUFACTURER') {
               tr = `<tr>
-                      <td>`+value+`</td>
+                      <td>`+ value + `</td>
                       <td><span class="badge badge-primary">Processando</span></td>
                       <td><span class="badge badge-secondary">Indisponível</span> </td>
                       <td><span class="badge badge-secondary">Indisponível</span> </td>
@@ -481,7 +481,7 @@ function buildChainsTable() {
                     </tr>`;
             } else if (result == 'DISTRIBUTOR') {
               tr = `<tr>
-                      <td>`+value+`</td>
+                      <td>`+ value + `</td>
                       <td><span class="badge badge-success">Concluído</span></td>
                       <td><span class="badge badge-warning">Processando</span> </td>
                       <td><span class="badge badge-secondary">Indisponível</span> </td>
@@ -489,7 +489,7 @@ function buildChainsTable() {
                     </tr>`;
             } else if (result == 'DEALER') {
               tr = `<tr>
-                      <td>`+value+`</td>
+                      <td>`+ value + `</td>
                       <td><span class="badge badge-success">Concluído</span></td>
                       <td><span class="badge badge-success">Concluído</span> </td>
                       <td><span class="badge badge-warning">processando</span> </td>
@@ -497,7 +497,7 @@ function buildChainsTable() {
                     </tr>`;
             } else if (result == 'DONE') {
               tr = `<tr>
-                      <td>`+value+`</td>
+                      <td>`+ value + `</td>
                       <td><span class="badge badge-success">Concluído</span></td>
                       <td><span class="badge badge-success">Concluído</span> </td>
                       <td><span class="badge badge-success">Concluído</span> </td>
@@ -518,41 +518,41 @@ function buildChainsTable() {
   });
 }
 
-$('#btnAdicionarProdutor').click(function(){
+$('#btnAdicionarProdutor').click(function () {
   var prodId = $('#prodId').val();
   var name = $('#name').val();
   var category = $('#category').val();
   var description = $('#description').val();
   var weight = $('#weight').val();
-  globContract.setProducer(prodId, name, category, description, weight, {from: $('#carteira').val(), gas: 3000000});
-}); 
+  globContract.setProducer(prodId, name, category, description, weight, { from: $('#carteira').val(), gas: 3000000 });
+});
 
-$('#btnAdicionarFabricante').click(function(){
+$('#btnAdicionarFabricante').click(function () {
   var fabrId = $('#FabrId').val();
   var productF = $('#productF').val();
   var shelfLife = $('#shelfLife').val();
   var packageType = $('#packageType').val();
-  globContract.setManufacturer(fabrId, fabrId, productF, shelfLife, packageType, {from: $('#carteira').val(), gas: 3000000});
+  globContract.setManufacturer(fabrId, fabrId, productF, shelfLife, packageType, { from: $('#carteira').val(), gas: 3000000 });
 });
 
-$('#btnAdicionarDistribuidor').click(function(){
+$('#btnAdicionarDistribuidor').click(function () {
   var distId = $('#DistId').val();
   var recipient = $('#recipient').val();
   var estimatedReceivingDate = $('#estimatedReceivingDate').val();
   var transportType = $('#transportType').val();
-  globContract.setDistributor(distId, distId, recipient, estimatedReceivingDate, transportType, {from: $('#carteira').val(), gas: 3000000});
+  globContract.setDistributor(distId, distId, recipient, estimatedReceivingDate, transportType, { from: $('#carteira').val(), gas: 3000000 });
 });
 
-$('#btnAdicionarRevendedor').click(function(){
+$('#btnAdicionarRevendedor').click(function () {
   var revId = $('#RevId').val();
   var resellerName = $('#resellerName').val();
   var storageLocation = $('#storageLocation').val();
   var temperature = $('#temperature').val();
-  globContract.setDealer(revId, revId, resellerName, storageLocation, temperature, {from: $('#carteira').val(), gas: 3000000});
+  globContract.setDealer(revId, revId, resellerName, storageLocation, temperature, { from: $('#carteira').val(), gas: 3000000 });
 });
 
 //Preencher Modal com a cadeia
-$('#btnAnunciar').click(function(){
+$('#btnAnunciar').click(function () {
   var _chainId = $('#chainId').val();
 
   $("#producer").html("");
@@ -563,72 +563,72 @@ $('#btnAnunciar').click(function(){
   globContract.getProducerById(_chainId, function (error, result) {
     if (!error) {
       var card =
-         '<div class="card">' +
-         '  <div class="card-body">' +
-         '    <p class="card-text"> Nome: ' + result[0] + '</p>' +
-         '    <p class="card-text"> Categoria: ' + result[1] + '</p>' +
-         '    <p class="card-text"> Descrição: ' + result[2] + '</p>' +
-         '    <p class="card-text"> Peso: ' + result[3] + '</p>' +
-         '  </div>' +
-         '</div>' +
-         '<i class="fa fa-angle-double-down" style="font-size:36px;"></i>';
-       $('#producer').append(card);
-     } else {
-       console.log("else");
-       console.log('error => ', error);
-     }
+        '<div class="card">' +
+        '  <div class="card-body">' +
+        '    <p class="card-text"> Nome: ' + result[0] + '</p>' +
+        '    <p class="card-text"> Categoria: ' + result[1] + '</p>' +
+        '    <p class="card-text"> Descrição: ' + result[2] + '</p>' +
+        '    <p class="card-text"> Peso: ' + result[3] + '</p>' +
+        '  </div>' +
+        '</div>' +
+        '<div style="text-align: center;"><i class="fa fa-angle-double-down fa-2x"></i></div>';
+      $('#producer').append(card);
+    } else {
+      console.log("else");
+      console.log('error => ', error);
+    }
   });
 
   globContract.getManufacturerById(_chainId, function (error, result) {
     if (!error) {
       var card =
-         '<div class="card">' +
-         '  <div class="card-body">' +
-         '    <p class="card-text"> Novo Produto: ' + result[0] + '</p>' +
-         '    <p class="card-text"> Validade: ' + result[1] + '</p>' +
-         '    <p class="card-text"> Tipo de Embalagem: ' + result[2] + '</p>' +
-         '  </div>' +
-         '</div>' +
-         '<i class="fa fa-angle-double-down" style="font-size:36px"></i>';
-       $('#manufacturer').append(card);
-     } else {
-       console.log("else");
-       console.log('error => ', error);
-     }
+        '<div class="card">' +
+        '  <div class="card-body">' +
+        '    <p class="card-text"> Novo Produto: ' + result[0] + '</p>' +
+        '    <p class="card-text"> Validade: ' + result[1] + '</p>' +
+        '    <p class="card-text"> Tipo de Embalagem: ' + result[2] + '</p>' +
+        '  </div>' +
+        '</div>' +
+        '<div style="text-align: center;"><i class="fa fa-angle-double-down fa-2x"></i></div>';
+      $('#manufacturer').append(card);
+    } else {
+      console.log("else");
+      console.log('error => ', error);
+    }
   });
 
   globContract.getDistributorById(_chainId, function (error, result) {
     if (!error) {
       var card =
-         '<div class="card">' +
-         '  <div class="card-body">' +
-         '    <p class="card-text"> Recipiente: ' + result[0] + '</p>' +
-         '    <p class="card-text"> Data Estimada De Entrega: ' + result[1] + '</p>' +
-         '    <p class="card-text"> Temperatura do Local: ' + result[2] + '</p>' +
-         '  </div>' +
-         '</div>' +
-         '<i class="fa fa-angle-double-down" style="font-size:36px"></i>';
-       $('#distributor').append(card);
-     } else {
-       console.log("else");
-       console.log('error => ', error);
-     }
+        '<div class="card">' +
+        '  <div class="card-body">' +
+        '    <p class="card-text"> Recipiente: ' + result[0] + '</p>' +
+        '    <p class="card-text"> Data Estimada De Entrega: ' + result[1] + '</p>' +
+        '    <p class="card-text"> Temperatura do Local: ' + result[2] + '</p>' +
+        '  </div>' +
+        '</div>' +
+        '<div style="text-align: center;"><i class="fa fa-angle-double-down fa-2x"></i></div>';
+      $('#distributor').append(card);
+    } else {
+      console.log("else");
+      console.log('error => ', error);
+    }
   });
 
   globContract.getDealerById(_chainId, function (error, result) {
     if (!error) {
       var card =
-         '<div class="card">' +
-         '  <div class="card-body">' +
-         '    <p class="card-text"> Revendedor: ' + result[0] + '</p>' +
-         '    <p class="card-text"> Local Armazenamento: ' + result[1] + '</p>' +
-         '    <p class="card-text"> Temperatura do Local: ' + result[2] + '</p>' +
-         '  </div>' +
-         '</div>';
-       $('#dealer').append(card);
-     } else {
-       console.log("else");
-       console.log('error => ', error);
-     }
+        '<div class="card">' +
+        '  <div class="card-body">' +
+        '    <p class="card-text"> Revendedor: ' + result[0] + '</p>' +
+        '    <p class="card-text"> Local Armazenamento: ' + result[1] + '</p>' +
+        '    <p class="card-text"> Temperatura do Local: ' + result[2] + '</p>' +
+        '  </div>' +
+        '</div>';
+      $('#dealer').append(card);
+    } else {
+      console.log("else");
+      console.log('error => ', error);
+    }
   });
 });
